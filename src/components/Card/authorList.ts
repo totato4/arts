237d3f22d780efb353d987ae9519802ae1e2f12b
totato4 <v4.97@list.ru>
2025-01-28 08:57:44 +0300

    add the lists of author and location

---
+++ src/components/Card/authorList.ts
@@ -0,0 +1,14 @@
+const Author = [
+  "",
+  "Ivan Aivazovsky",
+  "François Boucher",
+  "Leonardo Da Vinci",
+  "Edvard Munch",
+  "Rembrandt",
+  "Vincent van Gogh",
+  "BOTTICELLI, Sandro",
+  "Ivan Shishkin",
+  "Jean-François de Troy",
+];
+
+export default Author;
